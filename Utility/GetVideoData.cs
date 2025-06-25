10229960b99093a351e69c4648829b03650eb14d
Marcin Ryba <marcinroffice@gmail.com>
2025-06-25 14:04:21 +0200

    Button for video compression

---
+++ Utility/GetVideoData.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UtilityApplication.Settings;
+
+namespace UtilityApplication.Utility;
+
+public class GetVideoData
+{
+    public static TimeSpan GetVideoDuration(string videoPath)
+    {
+        var ffmpegPath = Path.Combine(DownloadConfig.FfmpegLocation, "ffprobe.exe");
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = ffmpegPath,
+            Arguments = $"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{videoPath}\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+
+        using var process = Process.Start(startInfo);
+        if (process != null)
+        {
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            if (double.TryParse(output.Trim(), out double seconds))
+            {
+                return TimeSpan.FromSeconds(seconds);
+            }
+        }
+
+        throw new Exception("Could not retrieve video duration.");
+    }
+
+}
